83668a2f70dfdd8eeb36fb388bd5fdae6494c558
Savid XXI <savidoficial09@gmail.com>
2023-09-15 06:08:00 -0500

    - Return routes

--- src/axios/skills/client.js
+++ src/axios/skills/client.js
@@ -7,13 +7,11 @@ const url = `${config.api.host}/${config.api.tessi_mock_api}`
 export const getAllSkills = async () => {
 
   try {
-    const response = await axios.get(`https://ec2-54-172-236-56.compute-1.amazonaws.com:8000/skills/`, {
-
+    const response = await axios.get(`${url}/skills/`, {
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
       }
-
     });
     return response.data.data;
   } catch (error) {
